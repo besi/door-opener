37c5b5a55fe21b279bea4c4c345a551c195ed141
Beat Besmer <beat.besmer@gmail.com>
2022-11-25 15:18:39 +0100

    Add submodules and initial pcb

--- sw/main.py
+++ sw/main.py
@@ -3,14 +3,14 @@ import time
 
 input_pin = 25
 output_pin = 15
-input = Pin(input_pin) # Button B
+input = Pin(input_pin, Pin.IN) # Button B
 output = Pin(output_pin, Pin.OUT)
 
 output.value(0)
 
 def valueChanged(pin):
     global input
-    
+    print("value changed")
     if input.value():
         print("Switch on relais")
         output.value(1)
@@ -18,3 +18,7 @@ def valueChanged(pin):
         output.value(0)
 
 input.irq(valueChanged)
+
+while True:
+    time.sleep(.2)
+
